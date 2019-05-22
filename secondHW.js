cfbe9fce84f9133f0050e4944f6e6980bd30e4c9
Olga <olastardust@gmail.com>
2019-05-22 22:24:10 +0300

    second hw

---
+++ secondHW.js
@@ -0,0 +1,21 @@
+//first task
+
+var letters = []
+var arr = "Backend As A Service".split(' ')
+for(i=0; i<arr.length; i++) {
+    letters += arr[i].substr(0,1)
+}
+var text = letters.push
+console.log(letters)
+
+//second task
+var newData = new Date()
+if (newData = String) {
+  console.log(newData);
+} else {
+  console.log("Неверный тип данных");
+}
+
+// мне кажется, что я не понимаю, как с русского языка переводить в код.
+// т.е. теорию читаю и, в принципе, понимаю, а вот потом с задачей связать не могу
+// что посоветуете? 
